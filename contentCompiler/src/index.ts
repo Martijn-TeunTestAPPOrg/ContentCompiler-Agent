4c1957d8708b74be678c34b6798878a998413987
Martijn Schuman <martijnschuman@hotmail.com>
2025-02-21 19:31:44 +0100

    Docker setup

--- contentCompiler/src/index.ts
+++ contentCompiler/src/index.ts
@@ -1,3 +1,4 @@
+import { pino } from "pino";
 import { Probot } from "probot";
 import { isAppCommit } from "./helpers.js";
 import { preCompile } from "./preCompile.js";
@@ -5,6 +6,16 @@ import { mainCompile } from "./mainCompile.js";
 
 
 export default (app: Probot) => {
+	// Override Probot's default logger to remove `req`
+	app.log = pino({
+		level: "info",
+		serializers: {
+			req: () => undefined, // Removes `req` from logs
+			res: pino.stdSerializers.res,
+			err: pino.stdSerializers.err
+		}
+	});
+
 	// Main compile
 	app.on("push", async (context) => {
 		// Skip if this is a commit from our app
